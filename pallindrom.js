0a820744c1740a38e23f9ad1842b26ba5efda9fc
unknown <pariharansh123@.com>
2022-11-30 19:57:45 +0530

    code written

---
+++ pallindrom.js
@@ -0,0 +1,12 @@
+let str="nitin";
+let bag="";
+for(let i=str.length-1;i>=0;i--)
+  {
+    bag=str[i]
+  }
+if(bag==str)
+{
+  console.log("String is pallindrom")
+}else{
+  console.log("String is not pallindrom")
+}
---
+++ prime.js
@@ -0,0 +1,16 @@
+let n=11;
+let count=0;
+for(let i=0;i<=n;i++)
+  {
+    if(n%2==0)
+    {
+      count++;
+    }
+  }
+if(count==2)
+{
+  console.log("Number is Prime")
+}else
+{
+  console.log("Number is not Prime")
+}
